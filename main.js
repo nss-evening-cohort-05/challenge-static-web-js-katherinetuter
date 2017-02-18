7b82c6bd45a7f877b17c2c4a420753b4788ca080
katherine tuter <katherinetuter@gmail.com>
2017-02-18 11:42:01 -0600

    fixed issues from PR comment

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
 
-//Create a `tree` function that should build a pine tree out of a character in the Chrome dev tools console. 
+//pull info for tree height and character from input boxes to build the tree later
+
 var treeChar = document.getElementById("treeChar");
 var treeHeight = document.getElementById("treeHeight");
+
+//check if both input boxes are complete and alert the user if they are not
 
 function startTree() {
 	var charInput = document.getElementById("treeChar").value;
 	var heightInput = document.getElementById("treeHeight").value;
 	if(charInput == "" || charInput.length == 0 || charInput == null) {
-	alert("Please choose a character to build your tree");
+		alert("Please choose a character to build your tree");
 	}
 	else if(heightInput == "" || heightInput.length == 0 || heightInput == null) {
 		alert("Please choose a number of rows for the height of your tree");
 		}
 	else {	
-	var treeObject = { height: treeHeight.value*1,
-					   character: treeChar.value
-					 };
+		var treeObject = { height: treeHeight.value*1,
+					       character: treeChar.value
+					 	};
 	growYourTree(treeObject);
 	}
 }
+
+//grow the tree using the user's input from the webpage
 
 var growYourTree = function(treeBuilders) {
 	var treeString = "";
@@ -27,13 +32,16 @@ var growYourTree = function(treeBuilders) {
 	var treeSpace = treeBuilders.height - (i + 1);
 	console.log(" ".repeat(treeSpace) + treeBuilders.character.repeat(treeChars));
 	}
-console.log(treeString);
 }
 
  	
- //add an event listener to the grow tree button, as well as an event listener for the enter/return key.
+//add an event listener to the grow tree button to start to grow the tree
+
 var growButton = document.getElementById("growButton");
 growButton.addEventListener("click", startTree);
+
+//add an event listener to check if enter is pressed in either input field
+
 document.getElementById("treeInfo").addEventListener("keyup", function(event) {
     event.preventDefault();
     if (event.keyCode == 13) {
